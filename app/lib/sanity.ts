de0d5c8dc2c053ccbdfc91d7f280ae28a511bfaa
Danish <danisharyanfahim@gmail.com>
2024-12-05 00:28:03 -0500

    updated sanity studio ID to deployed version

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -4,7 +4,7 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   apiVersion: "2023-05-03",
   dataset: "production",
-  projectId: "66my11p9",
+  projectId: "4chmhms1",
   useCdn: false,
 });
 
