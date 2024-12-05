db9ea55995df64d73f549b8ec27b320630ca997c
Danish <danisharyanfahim@gmail.com>
2024-12-04 23:41:34 -0500

    update

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,5 +1,5 @@
-import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
+import { createClient } from "next-sanity";
 
 export const client = createClient({
   apiVersion: "2023-05-03",
